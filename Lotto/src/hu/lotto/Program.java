0624b9323108ebd662a5c31b1ef32e3ca85a0e55
Fekete Balázs <114759825+Khy007@users.noreply.github.com>
2023-09-02 20:49:31 +0200

    Frissítések és egyszerűsítések a kódban.
    Ver 1.03. FB

--- Lotto/src/hu/lotto/Program.java
+++ Lotto/src/hu/lotto/Program.java
@@ -9,17 +9,17 @@ import org.springframework.core.io.Resource;
 
 public class Program {
 
+	static Resource resource= new ClassPathResource("applicationContext.xml");
+	static BeanFactory factory = new XmlBeanFactory(resource);
+	
 	public static void main(String[] args) {
 		
-	
-		Resource resource= new ClassPathResource("applicationContext.xml");
-		BeanFactory factory = new XmlBeanFactory(resource);
 		
-		System.out.println("Válassz egy menüpontot:");
-		System.out.println("1. Lottószámok húzása");
-		System.out.println("2. Leggyakoribb 3 szám kigyűjtése");
-		System.out.println("3. Tömb kiiratása");
-		System.out.println("4. Kilépés");
+		System.out.println("Valassz egy menupontot:");
+		System.out.println("1. Lottoszamok huzasa");
+		System.out.println("2. Leggyakoribb 3 szam kigyujtese");
+		System.out.println("3. Tomb kiiratasa");
+		System.out.println("4. EXIT");
 		
 		Menu menu =(Menu)factory.getBean("menu");
 		menu.proc();
