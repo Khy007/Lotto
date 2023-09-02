ff980fae0b9967ba6bfaf8330d75c05b367c7e71
Fekete Balázs <114759825+Khy007@users.noreply.github.com>
2023-09-02 18:21:54 +0200

    DB változat 1.0 by FB

--- Lotto/src/hu/lotto/Program.java
+++ Lotto/src/hu/lotto/Program.java
@@ -1,5 +1,7 @@
 package hu.lotto;
 
+import java.sql.*;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -9,8 +11,15 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+	
 		Resource resource= new ClassPathResource("applicationContext.xml");
 		BeanFactory factory = new XmlBeanFactory(resource);
+		
+		System.out.println("Válassz egy menüpontot:");
+		System.out.println("1. Lottószámok húzása");
+		System.out.println("2. Leggyakoribb 3 szám kigyűjtése");
+		System.out.println("3. Tömb kiiratása");
+		System.out.println("4. Kilépés");
 		
 		Menu menu =(Menu)factory.getBean("menu");
 		menu.proc();
