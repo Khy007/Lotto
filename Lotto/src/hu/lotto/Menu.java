8c8c92f94bbad0310485e417824254d60d5a862e
Fekete Balázs <114759825+Khy007@users.noreply.github.com>
2023-09-02 20:56:36 +0200

    Kód tisztázás, felesleges sorok törlése.
    Ver 1.04. FB

--- Lotto/src/hu/lotto/Menu.java
+++ Lotto/src/hu/lotto/Menu.java
@@ -4,20 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class Menu {
 	
 	private String menuValaszto;
 	
-
 	public String getMenuValaszto() {
 		return menuValaszto;
 	}
@@ -31,35 +23,28 @@ public class Menu {
 		Scanner sc = new Scanner(System.in);
 		boolean exit = false;
 		while(!exit) {
-						
-			
-			
+		
 				int valasztas = sc.nextInt();		
 				switch(valasztas){
-				case 1:	
-					Huzas huzas=(Huzas)Program.factory.getBean("huzas");
-					huzas.proc();		
+					case 1:	
+						Huzas huzas=(Huzas)Program.factory.getBean("huzas");
+						huzas.proc();		
 					break;
-				case 2:
-					Kereses kereses=(Kereses)Program.factory.getBean("kereses");
-					System.out.println("A leggyakoribb "+kereses.getBestOf() +" szamok: " );
-					kereses.proc();      
-				  break;
-				case 3:
-					System.out.println("A tomb tartalma: " );
-					Kiolvas kiolvas=(Kiolvas)Program.factory.getBean("kiolvas");
-					kiolvas.proc(); 
 					
-/*					for (int i = 0; i < Huzas.getKihuzottSzamokEddig().size(); i++) {
-						System.out.print(String.format("%3d", Huzas.getKihuzottSzamokEddig().get(i)));
-					    if ((i + 1) % 5 == 0) {
-					        System.out.println();
-					    }
-					}
-*/				  break;
-				case 4:
-				  System.out.println("Viszont latasra :) ");
-				  
+					case 2:
+						Kereses kereses=(Kereses)Program.factory.getBean("kereses");
+						System.out.println("A leggyakoribb "+kereses.getBestOf() +" szamok: " );
+						kereses.proc();      
+					break;
+				
+					case 3:
+						System.out.println("A tomb tartalma: " );
+						Kiolvas kiolvas=(Kiolvas)Program.factory.getBean("kiolvas");
+						kiolvas.proc(); 
+					break;
+				
+					case 4:
+						System.out.println("Viszont latasra :) ");
 						try {
 							Connection dbconnect = DriverManager.getConnection(Huzas.szerverC, Huzas.szerverF, Huzas.szerverJL);		
 							System.out.println("SQL szerver CONNECTED!:)");
@@ -72,18 +57,18 @@ public class Menu {
 						}catch (SQLException e) {
 							System.out.println("Csatlakozas nem pipa :(");
 						}
+					exit = true;
+					break;
 					
-				  exit = true;
-				  break;
-				default:
-					System.out.println("Kérlek csak számokat használj!");
+					default:
+						System.out.println("Kérlek csak számokat használj!");
 			    	break;
 				}
-			}
+		}
 			
-			}else {
-				System.out.println("NEM LÉTEZŐ MENÜ OPCIÓ");
-			}
+	}else {
+		System.out.println("NEM LÉTEZŐ MENÜ OPCIÓ");
+	 }
 	
 	}
 
